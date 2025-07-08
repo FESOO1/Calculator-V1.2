7702a909f5bafbfa0ed06907204c59ca2b173a94
FES001 <firdavs7794948@gmail.com>
2025-07-08 21:27:52 +0500

    Working on it.

---
+++ script.js
@@ -0,0 +1,6 @@
+const numberKeys = document.querySelectorAll('.main-calculator-keyboard-number-button');
+const operatorKeys = document.querySelectorAll('.main-calculator-keyboard-operator-button');
+const deleteKey = document.querySelector('.main-calculator-keyboard-del-button');
+const resetKey = document.querySelector('.main-calculator-keyboard-reset-button');
+const calculateKey = document.querySelector('.main-calculator-keyboard-calculate-button');
+const dotKey = document.querySelector('.main-calculator-keyboard-dot-button');
